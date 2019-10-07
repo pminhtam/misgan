a208c00bfc7978c0673e8d1ec97e3c2d73ef3f16
pminhtam <pminhtamnb@gmail.com>
2019-10-07 20:52:32 +0700

    tam : fix gain2

--- gain_tpc_h2.py
+++ gain_tpc_h2.py
@@ -13,15 +13,19 @@ train_rate = 1.0
 
 
 data_file = "lineitem.tbl.8"
-Data = np.loadtxt(data_file, delimiter="|",skiprows=1,usecols = (0, 1, 2, 4, 5,6,7))[:cf.num_row,:]
+Data = np.loadtxt(data_file, delimiter="|",skiprows=1,usecols = (0, 1, 2, 4, 5))[:cf.num_row,:]
 # print(np.array([Data[:,0]+Data[:,1]]).T)
+
+Data = (Data - np.max(np.abs(Data),axis = 0)) / (np.max(np.abs(Data),axis = 0)+ 1e-10 - np.min(np.abs(Data),axis = 0))
+print(Data[0])
 Data = np.append(Data,np.array([Data[:,0]+Data[:,1]]).T,1)
 Data = np.append(Data,np.array([Data[:,2]+Data[:,3]]).T,1)
-Data = np.append(Data,np.array([Data[:,0]*Data[:,1]]).T,1)
-Data = np.append(Data,np.array([Data[:,2]*Data[:,3]]).T,1)
+# Data = np.append(Data,np.array([Data[:,0]*Data[:,1]]).T,1)
+# Data = np.append(Data,np.array([Data[:,2]*Data[:,3]]).T,1)
 
 Dim,Train_No,trainX,trainM = Data_Generate(Data)
-
+print(trainX[0])
+"""
 X,M,H,New_X,D_loss1,G_loss1,MSE_train_loss,MSE_test_loss,D_solver,G_solver,G_sample = make_model(Dim)
 
 # Sessions
@@ -45,3 +49,4 @@ plt.plot(train_loss_curr)
 plt.subplot(2, 1, 2)
 plt.plot(test_loss_curr)
 plt.savefig("./img/"+ data_file + "_gain.jpg")
+"""
