a5debbf8d85bb2351bd815ca0c979b582d92310a
thanhdat285 <dd.tt97@gmail.com>
2019-10-16 09:59:57 +0700

    Prob cols

--- gain/train_gain.py
+++ gain/train_gain.py
@@ -2,6 +2,7 @@ from gain.data_gain import Data_Generate
 from gain.model_gain import *
 import argparse
 
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--data-file",type=str,default = "data/uce_train.csv",help="path to data file")
@@ -24,7 +25,7 @@ def main():
     data_file = args.data_file
     Data = np.genfromtxt(data_file, delimiter=",", filling_values=0)
     Dim,Train_No,trainX,trainM = Data_Generate(Data,p_miss)
-    X,M,H,New_X,D_loss1,G_loss1,MSE_train_loss,MSE_test_loss,D_solver,G_solver,G_sample = make_model(Dim,alpha)
+    X,M,H,New_X,D_loss1,G_loss1,MSE_train_loss,MSE_test_loss,D_solver,G_solver,G_sample, prob_masks = make_model(Dim,alpha)
 
     # Sessions
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.3)
@@ -34,7 +35,7 @@ def main():
     sess.run(tf.global_variables_initializer())
     np.random.seed(cf.gain_seed)
     np.random.shuffle(trainX)
-    train_loss_curr,test_loss_curr = train(X,M,H,New_X,D_loss1,G_loss1,MSE_train_loss,MSE_test_loss,D_solver,G_solver,G_sample,Dim,Train_No,trainX,trainM,sess,gain_iter,batch_size,p_hint)
+    train_loss_curr,test_loss_curr = train(X,M,H,New_X,D_loss1,G_loss1,MSE_train_loss,MSE_test_loss,D_solver,G_solver,G_sample,Dim,Train_No,trainX,p_miss,sess,gain_iter,batch_size,p_hint,prob_masks)
 
     saver = tf.train.Saver()
     saver.save(sess, args.save_path)
--- gen_data.py
+++ gen_data.py
@@ -1,6 +1,7 @@
 import numpy as np 
 
-data = np.random.uniform(0, 100, size=(10000, 6))
+data = np.random.uniform(0, 100, size=(10000, 3))
 data[:,1] = 2*data[:,0]
+# data[:, 2] = np.random.normal(size=(10000,))
 
 np.savetxt("data/y=2x.csv", data, delimiter=",",fmt="%.3f")
